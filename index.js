e810cce9a43b1f52d66cecb7bc80abc11155d8d1
Arye Levin <aryelevin@me.com>
2021-04-13 00:09:44 +0300

    Updated devices tokens

--- index.js
+++ index.js
@@ -13,11 +13,9 @@ function PushedNotificationAccessory(log, config) {
 	this.log = log;
 	
 	this.tokensToSendTo = [
-							"e9ca6da3db7b7b8fa4a5c1abc28636c2d5f61d1fed921a04a1618363c819a939", // Arye's iPhone 11 Pro
 							"10acec37b7933d3499e7e3d2d26799c8294ebdd9cd4f01777df2c0a366cb50f6", // Arye's iPhone 12 Pro
-							"1ed60e4bbedef565ba6aba284c625b1479620e7ae66756e3e835f38425f4ca52", // Tehila's iPhone XS
+							"e467f7a676b2d3396488fcd158e061bf59141a706d86aab8266f9f342a058c27", // Tehila's iPhone 11 Pro
 							"a621a44c7afe11ae2c27569fc5213292d27dbe8b95944b9c519ad1422abca901", // Home's iPad Pro 12.9 1st Gen (Kitchen)
-							"49806c10fb7cc9a2d81dc1cd7506f19e715ec31f5e46b5a852e18e37699d9be2", // Arye's iPhone SE
 							"ae5ac8d8e850049180f830282fd2268b69fba183ab7cf24848b87f4c09f521b1", // Home's Music iPad (Living room)
 							"f88e03e7eaf84b43f6a1c471a45f582bb0711accdefd5de8c7e389b2b43d5b5e", // Home's iPad Pro 12.9 2nd Gen (Entrance)
 							"7b80590f7407fef532f9091351ec81d65c973a50f089179811a1e84a031d9ca0"  // Home's iPad mini 5th Gen (Bedroom)
